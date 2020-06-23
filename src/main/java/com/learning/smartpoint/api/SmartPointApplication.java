f24e94113f677f1d6aa442e39157ae07c9d6d2ff
Nicolas Guarino <guarino.nicolas.santana@gmail.com>
2020-06-23 15:26:12 -0300

    first commit

---
+++ src/main/java/com/learning/smartpoint/api/SmartPointApplication.java
@@ -0,0 +1,13 @@
+package com.learning.smartpoint.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPointApplication.class, args);
+	}
+
+}
